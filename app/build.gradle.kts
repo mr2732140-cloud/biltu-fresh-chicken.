8a9e212c7f426ed41fab34b351116acb47987edc
mr2732140-cloud <mr2732140@gmail.com>
2026-09-02 12:13:06 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,19 @@
 
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "com.biltufreshchicken.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.biltufreshchicken.app"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
